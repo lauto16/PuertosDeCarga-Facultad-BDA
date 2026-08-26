12aa776775e201dffe0c22cc61085efb91bee52c
lauto16 <lautymartino658@gmail.com>
2026-08-26 20:16:35 -0300

    initial

---
+++ puerto-de-carga/src/main/java/com/puerto-de-carga/App.java
@@ -0,0 +1,13 @@
+package com.puerto-de-carga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
